5c163d15b971ac337bcd1defa15ac6487dc72edd
drewpearce <drew@caffdev.com>
2018-01-10 22:12:09 -0500

    added sub commands, list, add, drop

--- legos/cryptocurrency.py
+++ legos/cryptocurrency.py
@@ -36,14 +36,123 @@ class Cryptocurrency(Lego):
             logger.error('''Could not identify message source in message:
                         {}'''.format(message))
 
-        if message['text'].split()[0] == '!hodl':
-            self.reply(message, 'HODL!', opts)
+        message_list = message['text'].split()
+        if message_list[0] == '!hodl':
+            self.reply(message, self._parse_multi_commands(message_list), opts)
         else:
             try:
                 query = message['text'].split()[1]
             except:
                 self.reply(message, "Invalid query", opts)
             self.reply(message, self._lookup_symbol(query), opts)
+
+    def _parse_multi_commands(self, message_list):
+        if len(message_list) == 1:
+            return self._lookup_multi()
+        elif len(message_list) > 1:
+            case_dict = {}
+            case_dict['list'] = self._list_hodl_symbols
+            case_dict['add'] = self._add_symbols
+            case_dict['drop'] = self._drop_symbols
+            return case_dict[message_list[1]](message_list)
+
+    def _list_hodl_symbols(self, message_list):
+        return_val = ''
+        return_val += 'Convert From:  ' + self.crypto_index['crypto_index']['fsyms'] + '\n'
+        return_val += 'Convert To:  ' + self.crypto_index['crypto_index']['tsyms'] + '\n'
+        return return_val
+
+    def _add_symbols(self, message_list):
+        if len(message_list) < 3:
+            return ('Please supply additional arguments, e.g.:\n'
+                    '!hodl add from BTC,LTC')
+        elif message_list[2] == 'from':
+            return self._add_from_symbols(message_list)
+        elif message_list[2] == 'to':
+            return self._add_to_symbols(message_list)
+        else:
+            return 'There was an issue processing your request.'
+
+    def _add_from_symbols(self, message_list):
+        try:
+            add_fsyms = message_list[3].split(',')
+            old_fsyms = self.crypto_index['crypto_index']['fsyms'].split(',')
+            new_fsyms = list(set(old_fsyms + add_fsyms))
+            self.crypto_index['crypto_index']['fsyms'] = ','.join(new_fsyms)
+            with open('crypto_index.ini', 'w') as configfile:
+                self.crypto_index.write(configfile)
+            return self._list_hodl_symbols(message_list)
+        except:
+            return ('Please supply additional arguments, e.g.:\n'
+                    '!hodl add from BTC,LTC')
+
+    def _add_to_symbols(self, message_list):
+        try:
+            add_tsyms = message_list[3].split(',')
+            old_tsyms = self.crypto_index['crypto_index']['tsyms'].split(',')
+            new_tsyms = list(set(old_tsyms + add_tsyms))
+            self.crypto_index['crypto_index']['tsyms'] = ','.join(new_tsyms)
+            with open('crypto_index.ini', 'w') as configfile:
+                self.crypto_index.write(configfile)
+            return self._list_hodl_symbols(message_list)
+        except:
+            return ('Please supply additional arguments, e.g.:\n'
+                    '!hodl add to BTC,LTC')
+
+    def _drop_symbols(self, message_list):
+        if len(message_list) < 3:
+            return ('Please supply additional arguments, e.g.:\n'
+                    '!hodl drop from BTC')
+        elif message_list[2] == 'from':
+            return self._drop_from_symbols(message_list)
+        elif message_list[2] == 'to':
+            return self._drop_to_symbols(message_list)
+        else:
+            return 'There was an issue processing your request.'
+
+    def _drop_from_symbols(self, message_list):
+        try:
+            drop_fsyms = message_list[3].split(',')
+            old_fsyms = self.crypto_index['crypto_index']['fsyms'].split(',')
+            new_fsyms = set(old_fsyms).symmetric_difference(set(drop_fsyms))
+            self.crypto_index['crypto_index']['fsyms'] = ','.join(new_fsyms)
+            with open('crypto_index.ini', 'w') as configfile:
+                self.crypto_index.write(configfile)
+            return self._list_hodl_symbols(message_list)
+        except:
+            return ('Please supply additional arguments, e.g.:\n'
+                    '!hodl drop from BTC,LTC')
+
+    def _drop_to_symbols(self, message_list):
+        try:
+            drop_tsyms = message_list[3].split(',')
+            old_tsyms = self.crypto_index['crypto_index']['tsyms'].split(',')
+            new_tsyms = set(old_tsyms).symmetric_difference(set(drop_tsyms))
+            self.crypto_index['crypto_index']['tsyms'] = ','.join(new_tsyms)
+            with open('crypto_index.ini', 'w') as configfile:
+                self.crypto_index.write(configfile)
+            return self._list_hodl_symbols(message_list)
+        except:
+            return ('Please supply additional arguments, e.g.:\n'
+                    '!hodl drop to BTC,LTC')
+
+    def _lookup_multi(self):
+        multi_url = self._build_index_url()
+        get_multi = requests.get(multi_url)
+        if get_multi.status_code == requests.codes.ok:
+            api_response = json.loads(get_multi.text)
+            return self._parse_multi_price_response(api_response)
+        else:
+            return 'There was an error getting the data: ' + get_multi.text
+
+    def _parse_multi_price_response(self, api_response):
+        return_val = ''
+        for key, value in api_response.items():
+            return_val += key + ':  |  '
+            for key, value in value.items():
+                return_val += '{} {}  |  '.format(value, key)
+            return_val += '\n'
+        return return_val
 
     def _lookup_symbol(self, query):
         query = query.upper()
